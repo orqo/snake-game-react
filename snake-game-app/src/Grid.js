c1fba180df6fb2438d7cc2de40ceb6667d6d1a0e
hickeyo4 <orqo.hickey4@mail.dcu.ie>
2021-08-12 15:32:49 +0100

    Got the snake to move around using key presses

--- snake-game-app/src/Grid.js
+++ snake-game-app/src/Grid.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useInterval } from "./lib/utils.js";
 import "./Grid.css";
 
 class LinkedListNode {
@@ -25,26 +26,91 @@ const Direction = {
 
 const GRID_SIZE = 10;
 
-const createGrid = (GRID_SIZE) => {
-  let cellCounter = 1;
-  const grid = [];
-  for (let row = 0; row < GRID_SIZE; row++) {
-    const currentRow = [];
-    for (let col = 0; col < GRID_SIZE; col++) {
-      currentRow.push(cellCounter++);
-    }
-    grid.push(currentRow);
-  }
-  return grid;
+const startSnakeValues = (grid) => {
+  const rowSize = grid.length;
+  const colSize = grid[0].length;
+  const startingRow = Math.round(rowSize / 2);
+  const startingCol = Math.round(colSize / 3);
+  const startingCell = grid[startingRow][startingCol];
+  return {
+    row: startingRow,
+    col: startingCol,
+    cell: startingCell,
+  };
 };
 
 const Grid = () => {
   const [grid, setGrid] = useState(createGrid(GRID_SIZE));
-
+  const [score, setScore] = useState(0);
   // starting food cell is always placed in the middle right of the grid
-  const [foodCell, setFoodCell] = useState(48);
-  const [snakeCells, setSnakeCells] = useState(new Set([44]));
-  const [snake, setSnake] = useState(new LinkedList(44));
+  const [snake, setSnake] = useState(new LinkedList(startSnakeValues(grid)));
+  const [snakeCells, setSnakeCells] = useState(
+    new Set([snake.head.value.cell])
+  );
+  const [foodCell, setFoodCell] = useState(snake.head.value.cell + 5);
+  const [direction, setDirection] = useState(Direction.RIGHT);
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      handleKeydown(e);
+    });
+  });
+
+  useInterval(() => {
+    moveSnake();
+  }, 150);
+
+  const handleKeydown = (e) => {
+    const newDirection = getKeyPress(e.key);
+    const isValidDirection = newDirection !== "";
+    if (!isValidDirection) return;
+    setDirection(newDirection);
+  };
+
+  const moveSnake = () => {
+    const currHeadCoords = {
+      row: snake.head.value.row,
+      col: snake.head.value.col,
+    };
+
+    const nextHeadCoords = getDirectionCoords(currHeadCoords, direction);
+    if (isOutOfBounds(nextHeadCoords, grid)) {
+      handleGameOver();
+      return;
+    }
+
+    const nextHeadCell = grid[nextHeadCoords.row][nextHeadCoords.col];
+
+    const newHead = new LinkedListNode({
+      row: nextHeadCoords.row,
+      col: nextHeadCoords.col,
+      cell: nextHeadCell,
+    });
+
+    const currHead = snake.head;
+    snake.head = newHead;
+    currHead.next = newHead;
+
+    const newSnakeCells = new Set(snakeCells);
+    newSnakeCells.delete(snake.tail.value.cell);
+    newSnakeCells.add(nextHeadCell);
+
+    snake.tail = snake.tail.next;
+    if (snake.tail === null) {
+      snake.tail = snake.head;
+    }
+
+    setSnakeCells(newSnakeCells);
+  };
+
+  const handleGameOver = () => {
+    setScore(0);
+    const snakeValues = startSnakeValues(grid);
+    setSnake(new LinkedList(snakeValues));
+    setFoodCell(snakeValues.cell + 5);
+    setSnakeCells(new Set([snakeValues.cell]));
+    setDirection(Direction.RIGHT);
+  };
 
   return (
     <div className="grid">
@@ -58,6 +124,61 @@ const Grid = () => {
       ))}
     </div>
   );
+};
+
+const createGrid = (GRID_SIZE) => {
+  let cellCounter = 1;
+  const grid = [];
+  for (let row = 0; row < GRID_SIZE; row++) {
+    const currentRow = [];
+    for (let col = 0; col < GRID_SIZE; col++) {
+      currentRow.push(cellCounter++);
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+};
+
+const getDirectionCoords = (coords, direction) => {
+  if (direction === Direction.UP) {
+    return {
+      row: coords.row - 1,
+      col: coords.col,
+    };
+  }
+  if (direction === Direction.RIGHT) {
+    return {
+      row: coords.row,
+      col: coords.col + 1,
+    };
+  }
+  if (direction === Direction.DOWN) {
+    return {
+      row: coords.row + 1,
+      col: coords.col,
+    };
+  }
+  if (direction === Direction.LEFT) {
+    return {
+      row: coords.row,
+      col: coords.col - 1,
+    };
+  }
+};
+
+const isOutOfBounds = (coords, grid) => {
+  const { row, col } = coords;
+  if (row < 0 || col < 0) return true;
+  if (row >= grid.length || col >= grid[0].length) return true;
+  return false;
+};
+
+const getKeyPress = (key) => {
+  if (key === "w") return Direction.UP;
+  if (key === "d") return Direction.RIGHT;
+  if (key === "s") return Direction.DOWN;
+  if (key === "a") return Direction.LEFT;
+  return "";
 };
 
 // function to check whether cell is a normal cell or not
